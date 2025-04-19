ed9d3726138198c0410e100459feacf7ca6f0ae1
goodjunseon <pzs2001926@gmail.com>
2025-04-19 16:56:53 +0900

    UserFindAllDTO 작성, toDTO 작성완료, 회원 모든 정보 포함

--- book/src/main/java/com/junseon/book/domain/dto/user/UserFindAllDTO.java
+++ book/src/main/java/com/junseon/book/domain/dto/user/UserFindAllDTO.java
@@ -1,4 +1,41 @@
 package com.junseon.book.domain.dto.user;
 
+
+import com.junseon.book.domain.entity.User;
+import com.junseon.book.domain.enums.Role;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserFindAllDTO {
+
+    private Long userId;
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+    private LocalDateTime createdAt;
+    private Role role;
+
+
+
+    public static UserFindAllDTO toDTO(User user) {
+        UserFindAllDTO dto = new UserFindAllDTO();
+
+        dto.setUserId(user.getUserId());
+        dto.setEmail(user.getEmail());
+        dto.setPassword(user.getPassword());
+        dto.setName(user.getName());
+        dto.setPhone(user.getPhone());
+        dto.setCreatedAt(user.getCreatedAt());
+        dto.setRole(user.getRole());
+
+        return dto;
+    }
 }
